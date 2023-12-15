c1776e429cdca344be4b85c19a7dc3a8429dbb15
stepanm99 <stepan.melichar99@seznam.cz>
2023-12-15 17:59:55 +0100

    argument parsing done

--- philo/main.c
+++ philo/main.c
@@ -1,7 +1,27 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smelicha <smelicha@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 17:59:18 by smelicha          #+#    #+#             */
+/*   Updated: 2023/12/15 17:59:31 by smelicha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philosophers.h"
 
-int	ft_atoi(const char *str)
+void	print_data(t_data *data)
+{
+	printf("num_of_philos: %i\n", data->num_of_philos);
+	printf("eat: %i\n", data->eat);
+	printf("sleep: %i\n", data->sleep);
+	printf("die: %i\n", data->die);
+	printf("must_eat: %i\n", data->must_eat);
+}
+
+int	ft_atoi(const char *str, t_data *data)
 {
 	size_t	i;
 	int		negflag;
@@ -15,7 +35,7 @@ int	ft_atoi(const char *str)
 	while (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i + 1] == '-' || str[i + 1] == '+')
-			return (0);
+			error(data, 2);
 		if (str[i] == '-')
 			negflag = (-1);
 		i++;
@@ -28,9 +48,26 @@ int	ft_atoi(const char *str)
 	return (n * negflag);
 }
 
-void	arg_pars(t_data *data)
+void	arg_pars(t_data *data, int argc, const char **argv)
 {
-
+	if (argv[1])
+		data->num_of_philos = ft_atoi(argv[1], data);
+	else
+		error(data, 2);
+	if (argv[2])
+		data->die = ft_atoi(argv[2], data);
+	else
+		error(data, 2);
+	if (argv[3])
+		data->eat = ft_atoi(argv[3], data);
+	else
+		error(data, 2);
+	if (argv[4])
+		data->sleep = ft_atoi(argv[4], data);
+	else
+		error(data, 2);
+	if (argv[5] && argc == 6)
+		data->must_eat = ft_atoi(argv[5], data);
 }
 
 void	free_data(t_data *data)
@@ -44,7 +81,13 @@ void	error(t_data *data, int errno)
 	if (data)
 		free_data(data);
 	if (errno == 2)
-		printf("Wrong argument!\n./philo [number of philosophers] [miliseconds to die] [miliseconds to eat] [miliseconds to sleep] [how many times to eat]");
+	{
+		printf("Wrong argument!\n./philo [number of philosophers] ");
+		printf("[miliseconds to die] [miliseconds to eat] [miliseconds to");
+		printf(" sleep] + optional [how many times to eat]\n");
+	}
+	else if (errno == 3)
+		printf("Error while creating thread\n");
 	else if (errno == 1)
 		printf("Memory allocation error!\n");
 }
@@ -53,11 +96,13 @@ int	main(int argc, const char **argv)
 {
 	t_data	*data;
 	
+	printf("argc: %i\n", argc);
 	data = NULL;
 	data = malloc(sizeof(t_data));
 	if (!data)
 		error(data, 1);
-	arg_pars(data);
+	arg_pars(data, argc, argv);
+	print_data(data);
 	free_data(data);
 	return (0);
 }
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smelicha <smelicha@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 17:59:11 by smelicha          #+#    #+#             */
+/*   Updated: 2023/12/15 17:59:14 by smelicha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
@@ -8,7 +20,23 @@
 
 typedef struct s_data
 {
+	int	num_of_philos;
 	int	eat;
 	int	sleep;
 	int	die;
+	int	must_eat;
 }	t_data;
+
+
+/*Utilities*/
+int		ft_atoi(const char *str, t_data *data);
+
+/*Data handling*/
+void	arg_pars(t_data *data, int argc, const char **argv);
+void	free_data(t_data *data);
+
+/*Error*/
+void	error(t_data *data, int errno);
+
+/*Debug*/
+void	print_data(t_data *data);
