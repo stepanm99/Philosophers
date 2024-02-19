9443d17a7ecbf23596790158e08ea08ff5cf155b
stepanm99 <stepan.melichar99@seznam.cz>
2024-02-19 17:40:41 +0100

    Fixing norm in philosopher_fork_handling c file

--- philo/philosopher_fork_handling.c
+++ philo/philosopher_fork_handling.c
@@ -6,7 +6,7 @@
 /*   By: smelicha <smelicha@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 20:51:28 by smelicha          #+#    #+#             */
-/*   Updated: 2024/02/18 23:25:10 by smelicha         ###   ########.fr       */
+/*   Updated: 2024/02/19 17:39:57 by smelicha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ void	ft_right_first_fork_lock(t_data *data, int p_num)
 	{
 		pthread_mutex_lock(data->philos[p_num].right_sfgrd);
 		pthread_mutex_lock(data->philos[p_num].left_sfgrd);
-		if (*data->philos[p_num].left_fork == 0 && *data->philos[p_num].right_fork == 0)
+		if (*data->philos[p_num].left_fork == 0 
+			&& *data->philos[p_num].right_fork == 0)
 		{
 			*data->philos[p_num].left_fork = 1;
 			ft_print_take_fork(data, p_num);
@@ -52,7 +53,8 @@ void	ft_left_first_fork_lock(t_data *data, int p_num)
 	{
 		pthread_mutex_lock(data->philos[p_num].left_sfgrd);
 		pthread_mutex_lock(data->philos[p_num].right_sfgrd);
-		if (*data->philos[p_num].left_fork == 0 && *data->philos[p_num].right_fork == 0)
+		if (*data->philos[p_num].left_fork == 0
+			&& *data->philos[p_num].right_fork == 0)
 		{
 			*data->philos[p_num].left_fork = 1;
 			ft_print_take_fork(data, p_num);
@@ -69,34 +71,3 @@ void	ft_left_first_fork_lock(t_data *data, int p_num)
 		ft_usleep(data->eat / 10);
 	}
 }
-
-
-
-// /// @brief Function that locks forks for philosopher, taking left first
-// /// @param data Main data struct
-// /// @param p_num Number of philosopher
-// void	ft_left_first_fork_lock(t_data *data, int p_num)
-// {
-// 	if (ft_check_state(data, p_num))
-// 		return ;
-// 	while (1)
-// 	{
-// 		pthread_mutex_lock(data->philos[p_num].left_sfgrd);
-// 		if (!*data->philos[p_num].left_fork)
-// 			*data->philos[p_num].left_fork = 1;
-// 		pthread_mutex_unlock(data->philos[p_num].left_sfgrd);
-// 		pthread_mutex_lock(data->philos[p_num].right_sfgrd);
-// 		if (!*data->philos[p_num].right_fork)
-// 		{
-// 			*data->philos[p_num].right_fork = 1;
-// 			if (!ft_check_state(data, p_num))
-// 				ft_print_take_fork(data, p_num);
-// 			pthread_mutex_unlock(data->philos[p_num].right_sfgrd);
-// 			break ;
-// 		}
-// 		else
-// 			*data->philos[p_num].left_fork = 0;
-// 		pthread_mutex_unlock(data->philos[p_num].right_sfgrd);
-// 		ft_usleep(data->eat / 8);
-// 	}
-// }
